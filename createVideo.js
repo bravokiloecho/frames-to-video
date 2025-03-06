42c29d3cee86b3424340c5b079f75e98f39b411d
Benjamin E <ben@elwyn.co>
2025-03-06 16:19:49 +0000

    add some more params

--- createVideo.js
+++ createVideo.js
@@ -8,7 +8,10 @@ const DEFAULTS = {
 	imagePattern: 'image-%05d.png',
 	outputVideo: './output/output.mp4',
 	fps: 24,
-	imageDuration: 2,
+	imageDuration: 1,
+	bitrate: '3M',
+	crf: 18,
+	preset: 'slow',
 }
 
 // Parse command-line arguments using yargs
@@ -43,6 +46,24 @@ const argv = yargs(hideBin(process.argv))
 		description: 'Duration each image should display (in seconds)',
 		default: DEFAULTS.imageDuration,
 	})
+	.option('bitrate', {
+		alias: 'b',
+		type: 'string',
+		description: 'Video bitrate, e.g., "3M" for 3 Mbps',
+		default: DEFAULTS.bitrate,
+	})
+	.option('crf', {
+		alias: 'c',
+		type: 'number',
+		description: 'Constant Rate Factor for quality (lower is better)',
+		default: DEFAULTS.crf,
+	})
+	.option('preset', {
+		alias: 'r',
+		type: 'string',
+		description: 'Encoding speed preset (e.g., "medium", "slow", "veryslow")',
+		default: DEFAULTS.preset,
+	})
 	.help().argv
 
 function createVideoFromImages(
@@ -51,10 +72,15 @@ function createVideoFromImages(
 	outputVideo,
 	fps,
 	imageDuration,
+	bitrate,
+	crf,
+	preset,
 ) {
 	// Calculate the effective frame rate for input images
 	const effectiveFramerate = 1 / imageDuration
-	const command = `ffmpeg -framerate ${effectiveFramerate} -i "${imagePath}/${imagePattern}" -vf "fps=${fps}" -c:v libx264 -pix_fmt yuv420p "${outputVideo}"`
+	const command = `ffmpeg -framerate ${effectiveFramerate} -i "${imagePath}/${imagePattern}" -vf "fps=${fps}" -c:v libx264 -b:v ${bitrate} -crf ${crf} -preset ${preset} -pix_fmt yuv420p "${outputVideo}"`
+  
+
   // Delete output video if it exists
   if (fs.existsSync(outputVideo)) {
     fs.unlinkSync(outputVideo)
@@ -81,6 +107,9 @@ const {
   outputVideo,
   fps,
   imageDuration,
+  bitrate,
+  crf,
+  preset,
 } = argv
 
 // Call the function with arguments from yargs
@@ -90,4 +119,7 @@ createVideoFromImages(
 	outputVideo,
 	fps,
 	imageDuration,
+	bitrate,
+	crf,
+	preset,
 )
